dfb366647134f7388f7e674ac4a9f0a4a9455809
eugene4545 <eugeneonuoha72@gmail.com>
2024-08-16 15:47:50 +0100

    lseeting up server config

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,4 +4,15 @@ import dotenv from "dotenv"
 import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
+/* ROUTE IMPORTS*/
 
+/* CONFIGURATIONS */ 
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors());
