8ef64301e912c640007387ea7da0aa048a58b597
eugene4545 <eugeneonuoha72@gmail.com>
2024-08-18 10:47:41 +0100

    setting up dashboard metrics

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,11 +1,24 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
-import build from "next/dist/build";
+
+export 
+export interface DashboardMetrics {
+  popularProducts: Product[];
+  salesSummary: SalesSummary[];
+  purchaseSummary: PurchaseSummary[];
+  expenseSummary: ExpenseSummary[];
+  expenseByCategorySummary: ExpenseByCategorySummary[];  
+}
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-    reducerPath: "api",
-    tagTypes: [],
-    endpoints: (build) => ({})
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  reducerPath: "api",
+  tagTypes: [DashboardMetrics],
+  endpoints: (build) => ({
+    getDashboardMetrics: build.query<DashboardMetrics, void>({
+      query: () => "/dashboard",
+      providesTags: ["DashboardMetrics"]
+    }),
+  }),
 });
 
 export const {} = api;
