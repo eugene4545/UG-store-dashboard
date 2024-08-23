2badf430fba606aaee0ec28dfe4f749b29808717
eugene4545 <eugeneonuoha72@gmail.com>
2024-08-23 10:47:18 +0100

    styling commit

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 const Dashboard = () => {
-  return ( 
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-      
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <div className="row-span-3 xl:row-span-6 bg-gray-500" />
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
