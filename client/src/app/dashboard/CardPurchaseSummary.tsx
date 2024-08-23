ded3bb601962537644b40312188e71b27cb7322d
eugene4545 <eugeneonuoha72@gmail.com>
2024-08-23 14:50:31 +0100

    populating dashboard --pop prod

---
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CardPurchaseSummary = (props: Props) => {
+  return (
+    <div>CardPurchaseSummary</div>
+  )
+}
+
+export default CardPurchaseSummary
---
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CardSalesSummary = (props: Props) => {
+  return (
+    <div>CardSalesSummary</div>
+  )
+}
+
+export default CardSalesSummary
