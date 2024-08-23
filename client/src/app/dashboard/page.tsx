76dca0981d8052ad18eeb2eaa17e326399c90aae
eugene4545 <eugeneonuoha72@gmail.com>
2024-08-23 10:45:21 +0100

    styling dashboard (setting up media breakpoints)

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+"use client"
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
+  return ( 
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+      
+    </div>
   )
 }
 
--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,6 +1,40 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export 
+export interface Product {
+    productId: string;
+    name: string;
+    price: number;
+    rating?: number;
+    stockQuantity: number;
+
+}
+
+export interface SalesSummary {
+    salesSummaryId: string;
+    totalValue: number;
+    changePercentage?: number;
+    date: string;
+}
+
+export interface PurchaseSummary {
+    purchaseSummaryId: string;
+    totalPurchase: number;
+    changePercentage: number;
+    date: string;
+}
+
+export interface ExpenseSummary {
+    expenseSummaryId: string;
+    totalExpenses: number;
+    date: string;
+}
+
+export interface ExpenseByCategorySummary {
+  expenseByCategorySummaryId: string;
+  category: string;
+  amount: string;
+  date: string;
+}
 export interface DashboardMetrics {
   popularProducts: Product[];
   salesSummary: SalesSummary[];
@@ -12,7 +46,7 @@ export interface DashboardMetrics {
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
   reducerPath: "api",
-  tagTypes: [DashboardMetrics],
+  tagTypes: ["DashboardMetrics"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
       query: () => "/dashboard",
@@ -21,4 +55,6 @@ export const api = createApi({
   }),
 });
 
-export const {} = api;
+export const {
+  useGetDashboardMetricsQuery,
+} = api;
