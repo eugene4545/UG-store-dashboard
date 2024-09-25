e5225759eb621353051950fa90fca12ed693bfbc
eugene4545 <eugeneonuoha72@gmail.com>
2024-09-25 19:50:45 +0100

    settings

---
+++ client/src/app/users/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useGetUsersQuery } from "@/state/api";
+import Header from "@/app/(components)/Header";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import UseAnimations from "react-useanimations";
+import alertOctagon from "react-useanimations/lib/alertOctagon";
+
+const columns: GridColDef[] = [
+  { field: "userId", headerName: "ID", width: 90 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "email", headerName: "email", width: 200 },
+];
+
+const Users = () => {
+  const { data: users, isError, isLoading } = useGetUsersQuery();
+
+  if (isLoading) {
+    return <div className="py-4">Loading...</div>;
+  }
+
+  if (isError || !users) {
+    return (
+      <div className=" flex items-center justify-center text-red-500 py-4">
+         <UseAnimations
+        animation={alertOctagon}
+        strokeColor="red"
+        size={36}
+        wrapperStyle={{ marginRight: '8px' }} // Adds spacing between icon and text
+      />
+      <span>Failed to fetch users</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      <Header name="Users" />
+      <DataGrid
+        rows={users}
+        columns={columns}
+        getRowId={(row) => row.userId}
+        checkboxSelection
+        className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
+      />
+    </div>
+  );
+};
+
+export default Users;
