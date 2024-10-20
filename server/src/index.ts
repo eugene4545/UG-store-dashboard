cd855ba123490f64470959c4e12b8773522e8e65
eugene4545 <eugeneonuoha72@gmail.com>
2024-10-20 12:53:37 +0100

    setup db

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,11 +8,23 @@ import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
+import { PrismaClient } from "@prisma/client";
+
 /* ROUTE IMPORTS*/
 
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
+const DATABASE_URL = process.env.DATABASE_URL || "postgresql://postgres:45701@localhost:5432/inventorymangement?schema=public";
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_URL,
+    },
+  },
+});
+
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
