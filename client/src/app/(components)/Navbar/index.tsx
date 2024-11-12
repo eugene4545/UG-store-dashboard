d61be5c84f8afd365376a0d9a13c1e17a4dab37e
eugene4545 <eugeneonuoha72@gmail.com>
2024-11-12 17:30:17 +0100

    navbar changes

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -138,7 +138,18 @@ const Navbar = () => {
             <Sun className="cursor-pointer text-gray-500" size={24} />
           )}
         </button>
-
+        {/* <div className="relative">
+                <Bell className="cursor-pointer text-gray-500" size={24} />
+                 <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
+                     3
+                 </span>
+             </div>
+             <hr className="w-0 h-7 border border-solid border-1 border-gray-300 mx-3"/>
+             <div className="flex items-center gap-3 cursor-pointer">
+              <div className="w-9 h-9">
+                 image
+               </div><span className="font-semibold">Akum</span>
+            </div> */}
         {/* Settings Button */}
         <Link href="/settings">
           <Settings className="cursor-pointer text-gray-500" size={24} />
